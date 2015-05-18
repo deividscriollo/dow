d727c9066b7a0fec57dbfe3994b08069e7097eee
Deivid <deividscriollo@gmail.com>
2015-05-18 10:54:31 -0500

    animacion

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ if(!isset($_SESSION))
 
 		<!-- text fonts -->
 		<link rel="stylesheet" href="dist/css/fontdc.css" />
+		<link rel="stylesheet" href="dist/css/animate.min.css" />
 		<link rel="stylesheet" href="dist/css/jquery.gritter.min.css" />
 
 
@@ -54,17 +55,16 @@ if(!isset($_SESSION))
 				<div class="row">
 					<div class="col-sm-10 col-sm-offset-1">
 						<div class="login-container">							
-							<div class="center"><img src="dist/images/banner.fw.png"></div>
+							<div class="center animated bounceInRight"><img src="dist/images/banner.fw.png"></div>
 
-							<div class="position-relative">
-								<div id="login-box" class="login-box visible widget-box no-border">
+							<div id="obj_animacion" class="position-relative">
+								<div id="login-box" class="login-box visible widget-box no-border animated bounceInLeft">
 									<div class="widget-body">
 										<div class="widget-main">											
 											<h4 class="header blue lighter bigger">
 												<i class="ace-icon fa fa-coffee green"></i>
 												Digíte su información
 											</h4>
-
 											<div class="space-6"></div>
 
 											<form id="form_proceso">
@@ -169,82 +169,6 @@ if(!isset($_SESSION))
 										</div>
 									</div><!-- /.widget-body -->
 								</div><!-- /.forgot-box -->
-
-								<div id="signup-box" class="signup-box widget-box no-border">
-									<div class="widget-body">
-										<div class="widget-main">
-											<h4 class="header green lighter bigger">
-												<i class="ace-icon fa fa-users blue"></i>
-												New User Registration
-											</h4>
-
-											<div class="space-6"></div>
-											<p> Enter your details to begin: </p>
-
-											<form>
-												<fieldset>
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="email" class="form-control" placeholder="Email" />
-															<i class="ace-icon fa fa-envelope"></i>
-														</span>
-													</label>
-
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
-															<i class="ace-icon fa fa-user"></i>
-														</span>
-													</label>
-
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
-													</label>
-
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Repeat password" />
-															<i class="ace-icon fa fa-retweet"></i>
-														</span>
-													</label>
-
-													<label class="block">
-														<input type="checkbox" class="ace" />
-														<span class="lbl">
-															I accept the
-															<a href="#">User Agreement</a>
-														</span>
-													</label>
-
-													<div class="space-24"></div>
-
-													<div class="clearfix">
-														<button type="reset" class="width-30 pull-left btn btn-sm">
-															<i class="ace-icon fa fa-refresh"></i>
-															<span class="bigger-110">Reset</span>
-														</button>
-
-														<button type="button" class="width-65 pull-right btn btn-sm btn-success">
-															<span class="bigger-110">Register</span>
-
-															<i class="ace-icon fa fa-arrow-right icon-on-right"></i>
-														</button>
-													</div>
-												</fieldset>
-											</form>
-										</div>
-
-										<div class="toolbar center">
-											<a href="#" data-target="#login-box" class="back-to-login-link">
-												<i class="ace-icon fa fa-arrow-left"></i>
-												Back to login
-											</a>
-										</div>
-									</div><!-- /.widget-body -->
-								</div><!-- /.signup-box -->
 							</div><!-- /.position-relative -->
 							
 						</div>
@@ -289,6 +213,14 @@ if(!isset($_SESSION))
 
 		<!-- inline scripts related to this page -->
 		<script type="text/javascript">
+			 
+			 jQuery(function($){
+			 	$('#obj_animacion').removeClass().addClass('animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			      $(this).removeClass();
+			    });
+			 });
+			 
+
 			jQuery(function($) {				
 			 $(document).on('click', '.toolbar a[data-target]', function(e) {
 				e.preventDefault();
@@ -297,9 +229,6 @@ if(!isset($_SESSION))
 				$(target).addClass('visible');//show target
 			 });
 			});
-			
-			
-			
 			//you don't need this, just used for changing background
 			jQuery(function($) {
 			 $('#btn-login-dark').on('click', function(e) {
@@ -323,7 +252,6 @@ if(!isset($_SESSION))
 				
 				e.preventDefault();
 			 });
-			 
 			});
 		</script>
 	</body>
