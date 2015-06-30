fa4b89a12d9075390c9958e194a042a25107052f
Oskrin <oskrs11@hotmail.com>
2015-06-30 16:37:29 -0500

    base

--- data/conexion.php
+++ data/conexion.php
@@ -2,14 +2,10 @@
     function conectarse() {
     	$conexion = null;
         try {                             
-<<<<<<< HEAD
 
-        // $conexion = pg_connect("dbname=d9v66gpljua1u9 host=ec2-54-163-227-94.compute-1.amazonaws.com port=5432 user=oygpguxkmdceak password=8f_RosNXtAkzfyLUyOsSuilKbN sslmode=require");
-        $conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=sisweb");
-=======
          //$conexion = pg_connect("dbname=d9v66gpljua1u9 host=ec2-54-163-227-94.compute-1.amazonaws.com port=5432 user=oygpguxkmdceak password=8f_RosNXtAkzfyLUyOsSuilKbN sslmode=require");
-        $conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=rootdow");
->>>>>>> origin/master
+        $conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=sisweb");
+
          if( $conexion == false )
             throw new Exception( "Error PostgreSQL ".pg_last_error() );         
         } catch( Exception $e )
