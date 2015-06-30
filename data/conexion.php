5b32a6140218ff22c1c189694e23b5e9f4b9e126
thyrons <willynarvaez6@gmail.com>
2015-06-30 16:29:26 -0500

    cambios
    conexion

--- data/conexion.php
+++ data/conexion.php
@@ -2,11 +2,7 @@
     function conectarse() {
     	$conexion = null;
         try {                             
-<<<<<<< HEAD
-        //$conexion = pg_connect("dbname=d9v66gpljua1u9 host=ec2-54-163-227-94.compute-1.amazonaws.com port=5432 user=oygpguxkmdceak password=8f_RosNXtAkzfyLUyOsSuilKbN sslmode=require");
-=======
-        // $conexion = pg_connect("dbname=d9v66gpljua1u9 host=ec2-54-163-227-94.compute-1.amazonaws.com port=5432 user=oygpguxkmdceak password=8f_RosNXtAkzfyLUyOsSuilKbN sslmode=require");
->>>>>>> 8693bd0cb2b782882ac7b2640431c595055f85c7
+         //$conexion = pg_connect("dbname=d9v66gpljua1u9 host=ec2-54-163-227-94.compute-1.amazonaws.com port=5432 user=oygpguxkmdceak password=8f_RosNXtAkzfyLUyOsSuilKbN sslmode=require");
         $conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=rootdow");
          if( $conexion == false )
             throw new Exception( "Error PostgreSQL ".pg_last_error() );         
