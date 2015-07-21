c37a4bcd503803dbad44e955f6a13566a21d4349
Willy Narvaez <willynarvaez6@gmail.com>
2015-07-20 20:14:53 -0500

    cambios
    cambio

--- data/generales.js
+++ data/generales.js
@@ -859,19 +859,19 @@ function eliminar_fila2(){
 
 
 function agregar_fila_inventario(id_tabla,id_productos,codigo_producto,detalle_producto,precio_costo,stock,existencia,diferencia){
-var contador=0;
-var vect = new Array();     
-var cont=0;
-var repe = 0;
-var fila = 0;
-var diferencia = 0;
-$("#"+id_tabla+" tbody tr").each(function (index) {                                  
-    $(this).children("td").each(function (index) { 
-        contador++;                                 
-    });                                                         
-});
+    var contador=0;
+    var vect = new Array();     
+    var cont=0;
+    var repe = 0;
+    var fila = 0;
+    var diferencia = 0;
+    $("#"+id_tabla+" tbody tr").each(function (index) {                                  
+        $(this).children("td").each(function (index) { 
+            contador++;                                 
+        });                                                         
+    });
 
-if(contador == 0){
+    if(contador == 0){
     diferencia = (parseInt( $("#cantidad").val()) - Math.abs(parseInt( $("#stock").val())));
     $("#"+id_tabla+" tbody").append( "<tr>" +"<td align=center>" + id_productos +"</td>" +"<td align=center>" + codigo_producto + "</td>" +"<td align=center>" + detalle_producto +"</td>" +"<td align=center>" + precio_costo +"</td>" +"<td align=center>" + stock +"</td>" +"<td align=center>" + existencia +"</td>" +"<td align=center>" + diferencia + "</td>" +"<td align=center>" + "<div class=hidden-sm hidden-xs action-buttons> <a class='red dc_btn_accion tooltip-error' data-rel='tooltip' data-original-title='Eliminar'><i class='ace-icon fa fa-trash-o bigger-130' onclick='return eliminar_fila2(event)' ></i></a></div>" +"</tr>" ); 
     }else{
